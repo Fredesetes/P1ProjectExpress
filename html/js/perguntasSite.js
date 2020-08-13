a7518b21df88afac5130e1121f062db28055f6c5
Fabio <fredesetes@gmail.com>
2020-08-13 18:21:30 +0100

    Perguntas Done

--- html/js/perguntasSite.js
+++ html/js/perguntasSite.js
@@ -1,36 +1,34 @@
 $(function() {
-    let showQuestions = $("#showQuestions")
+    let tabelaPerguntas = $("#tabelaPerguntas")
     let tabelaResp = $("#tabelaRespostas tbody");
     let collapse = ($(".collapseRespostas"));
-    let modalChange = ($("#changeModal"));
+    let modalChange = ($("#modalChange"));
+    let changeAnswers = ($("#modalChange .answers"))
+    let count = 0;
+    let presentRow;
+    let priorId;
+    let len;
 
 
-
+    //Vai buscar todas as perguntas da base de dados
     $.get('http://localhost:3000/api/perguntas', function(data) {
         data.forEach((pergunta) => {
             adicionarQuestao(pergunta);
         });
     });
 
+    //Botão para adicionar uma resposta
     $('#newAnswer').click(function() {
+        event.preventDefault()
         if ($(".answer").length < 4) {
             let input = "<td><input class='answer' type='text'></td>"
             tabelaResp.append("<tr>" + input + "</tr>")
         } else {
-            console.log("mais nada")
+            $.notify("Impossível adicionar mais respostas", "warning")
         }
-
-        console.log($(".answer").length)
     })
 
-    //Mostrar respostas
-    /*tabela.on("click", ".answers", function() {
-        let id = $(this).attr("answerNr");
-        let perguntas = $(this).attr("answers")
-        console.log(perguntas)
-        console.log(id)
 
-    })*/
 
     //Adicionar pergunta e reposta
     $('#button').click(function() {
@@ -59,15 +57,71 @@ $(function() {
         }
     });
 
-    showQuestions.on("click", ".answers", function() {
-        let col = $(this).closest(".collapseAnswer")
-        let respostas = $(this).attr("answers")
+    //Apresentar menu de alteração de Perguntas e respostas
+    tabelaPerguntas.on("click", ".change", function() {
+        $(changeAnswers).html("")
+        let id = $(this).attr("questionNr");
+        priorId = id;
+        let thisRow = $(this).closest("tr");
+        presentRow = thisRow;
+        let length = $(this).attr("length");
+        len = length
 
-        console.log(respostas)
-        collapse.collapse('toggle')
+        modalChange.find(".questionNumber").val($(thisRow.find("td")[0]).text())
+        modalChange.find(".questionDescription").val($(thisRow.find("td")[1]).text())
+        for (let index = 0; index < length; index++) {
+            $(changeAnswers).append(`<input type="text" class="form-control answerChange">`)
+            $(changeAnswers).find('.answerChange').val($(`#tabelaPerguntas .test`).find(`.resposta${index}`).attr('descricao'))
+        }
+
+        modalChange.modal()
+
+
     })
 
-    showQuestions.on("click", ".del", function() {
+    $('.newAnswerChange').click(function() {
+        if (len < 4) {
+            let input = `<input type="text" class="form-control answerChange">`
+            changeAnswers.append(input)
+            len++
+        } else {
+            $.notify("Impossível adicionar mais respostas", "warning")
+        }
+    })
+
+    $('.saveChanges').click(function() {
+        let elements = $(".answerChange")
+        console.log(elements);
+        let resp = []
+        elements.each((i, element) => {
+            resp.push({
+                id: i + 1,
+                descricao: element.value
+            })
+        })
+        let pergunta = {
+            descricao: modalChange.find(".questionDescription").val(),
+            id: modalChange.find('.questionNumber').val(),
+            respostas: resp,
+
+        }
+        $.ajax({
+            url: 'http://localhost:3000/api/perguntas/' + priorId,
+            type: "PUT",
+            data: pergunta,
+            success: function(result) {
+                $.notify("Pergunta alterada com sucesso", "success")
+                presentRow.remove();
+                adicionarQuestao(pergunta);
+            }
+        })
+        modalChange.modal('hide')
+        console.log(pergunta);
+    })
+
+
+    //Elimina uma pergunta
+    tabelaPerguntas.on("click", ".del", function() {
         let rowRemove = $(this).closest(".pergunta");
         let id = $(this).attr("questionNr");
 
@@ -81,32 +135,34 @@ $(function() {
         });
     });
 
-    /*showQuestions.on("click", ".change", function() {
-
-        let thisRow = $(this).closest("tr");
-        presentRow = thisRow
-        priorId = $(thisRow.find("td")[0]).text()
-
-        modalChange.find(".questionNumber").val($(thisRow.find("td")[0]).text())
-        modalChange.find(".questionDescription").val($(thisRow.find("td")[1]).text())
-        modalChange.modal()
-    })*/
-
+    //Adiciona uma pergunta à tabela 
     function adicionarQuestao(pergunta) {
-        let colId = "<div class='col-sm'><p>" + pergunta.id + "</p></div>";
-        let colDescricao = "<div class='col-sm'><p>" + pergunta.descricao + "</p></div>";
-        console.log(pergunta);
+        count = count + 1
+        let tdId = "<td>" + pergunta.id + "</td>";
+        let tdDescricao = "<td>" + pergunta.descricao + "</td>";
 
-        let tdOptionDelete = "<div class='col-sm'><button type='button' questionNr='" + pergunta.id + "' class='btn btn-sm btn-danger del'>Delete</button></div>";
-        let tdOptionChange = "<div class='col-sm'><button type='button' data-target='#changeModal' questionNr='" + pergunta.id + "' class='btn btn-sm btn-warning change'>Change</button></div>";
-        let tdOptionRespostas = "<div class='col-sm collapseAnswer'><button type='button' data-toggle='collapse' data-target='#collapseRespostas' answerNr='" + pergunta.id + "' answers = '" + pergunta.respostas + "' class='btn btn-sm btn-primary answers'>Respostas</button></div>";
+        let tdOptionDelete = "<button type='button' questionNr='" + pergunta.id + "' class='btn btn-sm btn-danger del'>Delete</button></td>";
+        let tdOptionChange = `<button length=${typeof pergunta.respostas=== "undefined" ? 0 : pergunta.respostas.length} type='button' data-target='#changeModal' questionNr='${pergunta.id}' class='btn btn-sm btn-warning change'>Change</button>`;
 
-        showQuestions.append("<div class='row pergunta'>" + colId + colDescricao + "<div class='col-sm'><div class='row'>" + tdOptionRespostas + tdOptionChange + tdOptionDelete + "</div></div></div>");
+        let tdOptionRespostas = `<td><button type='button' class='btn btn-sm btn-success answers' data-target='.collapseAnswers${count}' data-toggle='collapse'>Respostas</button>`;
+
+
+
+        let tdRespostas;
+        if (typeof pergunta.respostas === "undefined") {
+            tdRespostas = "<td colspan='3'>Não existem respostas para esta pergunta</td>"
+        } else {
+            tdRespostas = "<td colspan='3'>";
+            for (let index = 0; index < pergunta.respostas.length; index++) {
+                tdRespostas += `<p class='resposta${index}' number='${pergunta.respostas[index].id}' descricao='${pergunta.respostas[index].descricao}'>${pergunta.respostas[index].id}: ${pergunta.respostas[index].descricao} </p>`
+                console.log(pergunta.respostas[index].descricao);
+            }
+            tdRespostas += "</td>"
+
+        }
+        let collapse = "<tr class='collapse test collapseAnswers" + count + "'>" + tdRespostas + "</tr>"
+
+        tabelaPerguntas.append("<tr class='pergunta'>" + tdId + tdDescricao + " <div class='btn-group' role='group'>" + tdOptionRespostas + tdOptionChange + tdOptionDelete + "</div></tr>" + collapse);
     };
 
-    /*function escreverResposta() {
-        let input = "<td><input class='answer' type='text'></td>"
-            //let submitAnswer = "<td><button type='button' class='btn btn-sm btn-primary '>Submit</button></td>"
-        tabelaResp.append("<tr>" + input + "</tr>")
-    }*/
 })
